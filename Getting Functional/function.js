ee535b6d4c639ce9daa56b56febf788e00cd5d3c
Saito232 <gonzalesmarcjason56@gmail.com>
2025-03-20 00:34:48 -0700

    BattleshipV2 and Functions

---
+++ Getting Functional/function.js
@@ -0,0 +1,34 @@
+var dogName = "Potchi";
+var dogWeight = 23;
+if (dogWeight > 20) {
+    console.log(dogName + " Says Bowow");
+}  else {
+    console.log(dogName + "Says Woof");
+}
+
+var dogName = "Hughie";
+var dogWeight = 10;
+if (dogWeight > 20) {
+    console.log(dogName + " Says Bowow");
+}  else {
+    console.log(dogName + "Says Woof");
+}
+
+var dogName = "Yumi";
+var dogWeight = 13;
+if (dogWeight > 20) {
+    console.log(dogName + " Says Bowow");
+}  else {
+    console.log(dogName + "Says Woof");
+}
+
+var dogName = "Chloe";
+var dogWeight = 17;
+if (dogWeight > 20) {
+    console.log(dogName + " Says Bowow");
+}  else {
+    console.log(dogName + "Says Woof");
+}
+
+bark("Potchi", 20);
+bark("Hughie", 30);
