06e58109abe862d9c6cbf9d5ff7bdc979177e996
unknown <gonzalesmarcjason56@gmail.com>
2025-03-24 00:11:09 -0700

    Function Activity 1

---
+++ Activities/Function - Activity/script.js
@@ -0,0 +1,12 @@
+function whatShallWear(temp) {
+    if (temp < 60) {
+        console.log("Wear a Jacket");
+    }else if (temp < 70) {
+        console.log("Wear a t-shirt");
+    }else {
+        console.log("wear a sweater");
+    }
+}
+    whatShallWear(80)
+    whatShallWear(60)
+    whatShallWear(50)
